712714b914a28dc253cf425e10652999f2fb53c3
chengyuan <xiaoe3504@126.com>
2024-02-04 15:43:37 +0800

    1.review sth

--- src/main/java/com/psy/demo/controller/Mp3Controller.java
+++ src/main/java/com/psy/demo/controller/Mp3Controller.java
@@ -2,26 +2,40 @@ package com.psy.demo.controller;
 
 import com.psy.demo.global.BaseException;
 import com.psy.demo.global.NotGlobalControllerAdvice;
-import com.psy.demo.utils.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * 直接用云文件不用这个了
+ */
 @RestController
-
+@Deprecated()
 public class Mp3Controller {
-    private static String path = "src/main/resources/static/music";
+
+    @Value("classpath*:/static/music/*")
+    private Resource[] mp3Resources;
+
+    public List<Resource> getAllResources() {
+        return Stream.of(mp3Resources).collect(Collectors.toList());
+    }
 
     @GetMapping("mp3/{id}")
     @NotGlobalControllerAdvice
     public void downloadMp3(@PathVariable("id") Integer id, HttpServletResponse response) {
         File file;
+        Resource resource = getAllResources().get(id);
         try {
-            file = FileUtils.getFile(path, id);
-        } catch (Exception e) {
+            file = new File(resource.getURL().getFile());
+        } catch (IOException e) {
             throw new BaseException(e);
         }
         response.setContentType("audio/mp3");
