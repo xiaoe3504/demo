86ade62157e6a49b23354534637ff8ddc19a86b6
沉水龙雀 <oncwnuCz7RBvUKJfBOvp2ZtRYeO4@git.weixin.qq.com>
2024-07-23 23:34:23 +0800

    1.review sth

--- src/main/java/com/psy/demo/controller/LoginController.java
+++ src/main/java/com/psy/demo/controller/LoginController.java
@@ -25,32 +25,12 @@ public class LoginController {
     }
 
     @PostMapping(path = "decode")
-    public FinalUserInfo decodeUserInfo(UserInfoRes res) {
-        mockUserInfoRes(res);
+    public FinalUserInfo decodeUserInfo(@RequestBody UserInfoRes res) {
         log.info("res:" + JSON.toJSONString(res));
         return loginService.decodeUserInfo(res);
 
     }
 
-    public static void mockUserInfoRes(UserInfoRes res) {
-        String encryptedData = "7LoFF6oImPRRD/bUCZO6jdIADdYRP7KkflF9EQTticYtxfky5RGFm5BvwxmAI61grAfU0nF0qClEifrLoeuvSWpI7R2sJIwSSFr7lSe864l0b86S5j1bxLQDWBo/sxcRWa0cNeXu7UwbnGAtuY4s/l7yikf3J3LL52eW0tdbCyf/d//66+LHoDgbCFIf53xxIYkqCsjaNHd/Hdu4NBGp712fACpR28mwei4b5b1l2VfRcsL/hIRL/QhcugtyOoGY7hVF5a40zn+2GWaooVkRdOIpzKxG37RZ5TotPAPtUjm5z/0IXwj+oYKF4xtg/Ctvz4FXHcfqhSppuWIx2P5DLBwOUeOi9pM2fANG633CHHWlwDsWfaFZJV+mC5wEGUNOrnP+4Xg2Mq638Njnl1P6/9wSuZoagyWiWF+foCWOkv+EW8sn5cylqY+nbV1tvhCh+tAppih38xt0AYv4nIaV8Q==";
-        res.setEncryptedData(encryptedData);
-        String iv = "mF0iSde5NcylirMqugYIJw==";
-        res.setIv(iv);
-        String sessionKey = "MbPFdfreHEMk/hOU6Fa6eg==";
-        res.setSessionKey(sessionKey);
-        UserInfo userInfo = mockUserInfo();
-        res.setUserInfo(userInfo);
-    }
 
-    @NotNull
-    private static  UserInfo mockUserInfo() {
-        UserInfo userInfo = new UserInfo();
-        String avatarUrl = "https://thirdwx.qlogo.cn/mmopen/vi_32/POgEwh4mIHO4nibH0KlMECNjjGxQUq24ZEaGT4poC6icRiccVGKSyXwibcPq4BWmiaIGuG1icwxaQX6grC9VemZoJ8rg/132";
-        userInfo.setAvatarUrl(avatarUrl);
-        String nickName = "微信用户";
-        userInfo.setNickName(nickName);
-        return userInfo;
-    }
 
 }
