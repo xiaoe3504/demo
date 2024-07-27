937a42a6ee1a9bc246e9abe9d404543c86101619
沉水龙雀 <oncwnuCz7RBvUKJfBOvp2ZtRYeO4@git.weixin.qq.com>
2024-07-28 00:00:53 +0800

    1.review sth

--- src/main/java/com/psy/demo/vo/res/IntelligentTestScaleVO.java
+++ src/main/java/com/psy/demo/vo/res/IntelligentTestScaleVO.java
@@ -4,6 +4,7 @@ package com.psy.demo.vo.res;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.psy.demo.dto.IntelligentTestScaleDTO;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 @Data
@@ -11,6 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 @AllArgsConstructor
 @Getter
 @Setter
+@Slf4j
 public class IntelligentTestScaleVO {
     private long id;
     private String name;
@@ -20,7 +22,7 @@ public class IntelligentTestScaleVO {
     private String hash;
     @JsonProperty("isFree")
     private boolean isFree;
-
+    private double price;
 
     public static IntelligentTestScaleVO genVOByDTO(IntelligentTestScaleDTO dto) {
         IntelligentTestScaleVO vo = new IntelligentTestScaleVO();
@@ -29,9 +31,22 @@ public class IntelligentTestScaleVO {
         vo.setTitle(dto.getTitle());
         vo.setSid(dto.getSid());
         vo.setHash(dto.getHash());
-        vo.setTextCnt(dto.getCnt() +"人参与");
+        vo.setTextCnt(dto.getCnt() + "人参与");
         String isFreeStr = dto.getIsFree().trim();
-        vo.setFree(StringUtils.isNotEmpty(isFreeStr) && isFreeStr.equals("免费"));
+        boolean isFree = StringUtils.isNotEmpty(isFreeStr) && isFreeStr.equals("免费");
+        vo.setFree(isFree);
+        double price = 0;
+        if (!isFree && StringUtils.isNotEmpty(isFreeStr)) {
+            String[] arr = isFreeStr.split(" ");
+            if (StringUtils.isNotEmpty(arr[0])) {
+                try {
+                    price = Double.parseDouble(arr[0]);
+                } catch (NumberFormatException e) {
+                    log.error("price parse double err:" + e.getMessage(), e);
+                }
+            }
+        }
+        vo.setPrice(price);
         return vo;
     }
 
